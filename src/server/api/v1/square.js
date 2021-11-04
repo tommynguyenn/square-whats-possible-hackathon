35b2dd052bde4d5e72427118e9b35df6554203f6
Tommy Nguyen <tnguuyen@outlook.com>
2021-11-04 01:27:02 -0700

    push coinbase stuff

--- src/server/api/v1/square.js
+++ src/server/api/v1/square.js
@@ -1,16 +1,22 @@
 const router = require( 'express' ).Router();
 const { v4: uuidv4 } = require('uuid');
-const axios = require('axios');
 const { Client, Environment } = require('square');
+const CoinbaseClient = require('coinbase').Client;
 
 const client = new Client({
   environment: Environment.Sandbox,
   accessToken: process.env.SQUARE_ACCESS_TOKEN,
 })
 
+const cbClient = new CoinbaseClient({ 
+	apiKey: process.env.CB_API_KEY, 
+	apiSecret: process.env.CB_API_SECRET,
+	strictSSL: false
+});
+
 const listGiftCards = async () => {
 	const response = await client.giftCardsApi.listGiftCards();
-	console.log(response);
+	// console.log(response);
 	return response;
 }
 
@@ -44,12 +50,42 @@ const createGiftCardActivity = async ( locationId ) => {
 	console.log(response);
 }
 
+const getAccounts = () => new Promise(function(resolve, reject) {
+	cbClient.getAccounts({}, (err, accounts) => {
+		if (err) { 
+			reject(err);
+		} else {
+			resolve(accounts.map( acc => ( {
+				name: acc.name,
+				currency: acc.currency,
+				balance: acc.balance,
+				id: acc.id
+			} ) ) );
+		}
+	});
+});
+
 router.get('/', async (req, res) => {
 	console.log('/api/square/ called');
 
 	const response = await listGiftCards();
 	// createGiftCard();
 	// createGiftCardActivity('LNAMS88P1QWF7');
+
+	// cbClient.getCurrentUser(function(err, user) {
+	// 	console.log(user);
+	// });
+	
+	const accounts = await getAccounts();
+	console.log(accounts);
+
+	cbClient.getAccount('a4a3a1c5-c3cc-5e0a-aa15-b2f63262655a', function(err, account) {
+		console.log(account);
+		account.createAddress(null, function(err, address) {
+			console.log(address);
+		});
+	});
+
 
 	if (response.statusCode === 200) {
 		const data = JSON.parse(response.body);

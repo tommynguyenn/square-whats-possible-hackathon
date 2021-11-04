7b748d33c3d6503ab72bf1a37123d8109dad8cf2
Andy Cheng <andyc@virtro.ca>
2021-11-03 19:14:47 -0700

    Created nav bar (still need to route them)

--- src/client/App.js
+++ src/client/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Onboarding from './js/Onboarding';
 import SquareBoxModal from './js/SquareBoxModal';
-import NavBar from "./js/NavBar"
+import NavBar from "./js/Navbar/NavBar"
 import './css/app.css';
 
 export default function App() {
 	return (
 		<div className="app">
-		<NavBar/>
+		<NavBar />
 		<Router>
 			<Route path="/">
 					<Onboarding />
